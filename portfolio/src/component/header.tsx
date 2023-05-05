78ae3e10611c1ca3844b7c6a087d6ca696ed0905
gregoire.ivars <gregoire.ivars@inetum.com>
2023-05-05 15:37:36 +0200

    feat: constante applicatif

--- portfolio/src/component/header.tsx
+++ portfolio/src/component/header.tsx
@@ -1,29 +1,24 @@
 import '../css/header.css';
+import { TEXT_HEADER_P1, TEXT_HEADER_P2, TEXT_HEADER_P3, TEXT_HEADER_P4, TEXT_HEADER_TITLE } from "../constante/constantes";
 export default function Header() {
-    
+
     return (
         <>
             <div id="sidebar">
                 <nav>
                     <ul className='navBody'>
-                        <a href={""}><h1>Portfolio</h1></a>
+                        <a href={""}><h1>{TEXT_HEADER_TITLE}</h1></a>
                         <li>
-                            <a href={"/home/1"}>Accueil</a>
+                            <a href={"/home/1"}>{TEXT_HEADER_P1}</a>
                         </li>
                         <li>
-                            <a href={"/home/1"}>Boutique</a>
+                            <a href={"/home/1"}>{TEXT_HEADER_P2}</a>
                         </li>
                         <li>
-                            <a href={"/home/1"}>Nouveauté</a>
+                            <a href={"/home/1"}>{TEXT_HEADER_P3}</a>
                         </li>
                         <li>
-                            <a href={"/home/1"}>À propos</a>
-                        </li>
-                        <li>
-                            <a href={"/home/1"}>Contact</a>
-                        </li>
-                        <li>
-                            <a className="profil" href={"/home/1"}>Profils</a>
+                            <a href={"/home/1"}>{TEXT_HEADER_P4}</a>
                         </li>
                     </ul>
                 </nav>
--- portfolio/src/constante/constantes.tsx
+++ portfolio/src/constante/constantes.tsx
@@ -1,2 +1,13 @@
+
+
+//Header
+export const TEXT_HEADER_TITLE = "Porfolio"
+export const TEXT_HEADER_P1 = "Page 1"
+export const TEXT_HEADER_P2 = "Page 2"
+export const TEXT_HEADER_P3 = "Page 3"
+export const TEXT_HEADER_P4 = "Page 4"
+
 //Footer
 export const TEXT_FOOTER = "© 2023 Grégoire IVARS - Home Made Porfolio"
+
+//home
